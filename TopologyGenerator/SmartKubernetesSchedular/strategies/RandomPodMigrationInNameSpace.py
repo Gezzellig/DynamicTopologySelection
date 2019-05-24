6c6466de349f5a0a90f1ba3d04fb4ffdc503e64f
Gezzellig <knaap@live.com>
2019-05-24 15:26:15 +0200

    working kill random pod algorithm

---
+++ TopologyGenerator/SmartKubernetesSchedular/strategies/RandomPodMigrationInNameSpace.py
@@ -0,0 +1,6 @@
+from SmartKubernetesSchedular.strategies.AbstractStratagy import AbstractStratagy
+
+
+class RandomPodMigration(AbstractStratagy):
+    def generate_improvement(self, settings):
+        pass
