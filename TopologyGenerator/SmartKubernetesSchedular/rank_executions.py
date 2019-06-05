002409c2fe078ea9d8f13ef1b77784f4755e4690
Gezzellig <knaap@live.com>
2019-06-05 16:36:11 +0200

    now the retrieved neo4j results have structure

---
+++ TopologyGenerator/SmartKubernetesSchedular/rank_executions.py
@@ -0,0 +1,10 @@
+def max_resource():
+    pass
+
+
+def rank_execution(execution):
+    pass
+
+
+def find_best_execution(executions):
+    pass
--- TopologyGenerator/SmartKubernetesSchedular/retrieve_executions.py
+++ TopologyGenerator/SmartKubernetesSchedular/retrieve_executions.py
@@ -1,7 +1,14 @@
+import datetime
 import math
+import sys
 
+import load_settings
 from Neo4jGraphDriver import disconnect_neo4j
 from initializer.neo4j_queries import execute_query_function
+
+
+class NoExecutionsFoundException(Exception):
+    pass
 
 
 def retrieve_executions_on_load(tx, load, delta_load):
@@ -11,25 +18,76 @@ def retrieve_executions_on_load(tx, load, delta_load):
                   load_min=load-delta_load, load_max=load+delta_load)
 
 
-def retrieve_cheapest_executions(load, delta_load, price_per_cpu, price_per_gb):
+def retrieve_cheapest_executions(load, delta_load, price_per_core, price_per_gb):
     executions = execute_query_function(retrieve_executions_on_load, load, delta_load)
     cheapest_cost = math.inf
-    cheapest_executions = []
+    cheapest_execution_ids = []
     for execution in executions.records():
         total_cost = 0
         for node in execution["nodes"]:
-            total_cost += node["cpu"]*price_per_cpu + node["memory"]*price_per_gb
+            total_cost += node["cpu"]*price_per_core + node["memory"]*price_per_gb
 
         if total_cost == cheapest_cost:
-            cheapest_executions.append(execution["id"])
+            cheapest_execution_ids.append(execution["id"])
         elif total_cost < cheapest_cost:
             cheapest_cost = total_cost
-            cheapest_executions = [execution["id"]]
-    return cheapest_executions
+            cheapest_execution_ids = [execution["id"]]
+    return cheapest_execution_ids
+
+
+def retrieve_full_execution_data(tx, execution_ids):
+    return tx.run("unwind $execution_ids as execution_id \
+                    match (e:Execution) where id(e) = execution_id \
+                    match (e) -[:HasNode]-> (n:Node) \
+                    match (n) -[r:Ran]-> (p:Pod) \
+                    with e, n, collect({name:r.name, generate_name:p.generate_name}) as pods \
+                    return id(e) as id, e.start_time.epochMillis as start_time, e.end_time.epochMillis as end_time, collect({name:n.name, cpu:n.cpu, pods:pods}) as nodes",
+                  execution_ids=execution_ids)
+
+
+def extract_node_from_data_node(data_node):
+    pods = {}
+    node_name = data_node["name"]
+    for data_pod in data_node["pods"]:
+        pods[data_pod["name"]] = {
+            "node_name": node_name,
+            "pod_generate_name": data_pod["generate_name"]
+        }
+    node = {
+        "cpu": data_node["cpu"],
+        "pods": pods
+    }
+    return node
+
+def retrieve_full_executions(execution_ids):
+    data_executions = execute_query_function(retrieve_full_execution_data, execution_ids)
+    executions = []
+    for data_execution in data_executions.records():
+        nodes = {}
+        for data_node in data_execution["nodes"]:
+            nodes[data_node["name"]] = extract_node_from_data_node(data_node)
+        executions.append({
+            "start_time": datetime.datetime.fromtimestamp(data_execution["start_time"]/1000),
+            "end_time": datetime.datetime.fromtimestamp(data_execution["end_time"]/1000),
+            "nodes": nodes
+        })
+    return executions
+
+
+def retrieve_executions(load, settings):
+    load_delta = settings["load_delta"]
+    price_per_core = settings["price_per_core"]
+    price_per_gb = settings["price_per_gb"]
+    cheapest_executions_ids = retrieve_cheapest_executions(load, load_delta, price_per_core, price_per_gb)
+    if len(cheapest_executions_ids) < 1:
+        raise NoExecutionsFoundException()
+    executions = retrieve_full_executions(cheapest_executions_ids)
+    return executions
 
 
 def main():
-    print(retrieve_cheapest_executions(10000, 1000, 2, 0.1))
+    settings = load_settings.load_settings(sys.argv[1])
+    print(retrieve_executions(10000, settings))
     disconnect_neo4j()
 
 
